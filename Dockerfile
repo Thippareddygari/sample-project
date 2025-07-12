b3a6d42c303296ef56d91222d7ffbe610cf624a3
kishor <kishorebsc12@gmail.com>
2025-07-12 13:38:53 +0530

    added new project

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+#FROM openjdk:17-jre-slim
+FROM openjdk:17-jdk-slim
+VOLUME /tmp
+#USER test
+COPY target/*.jar app.jar
+EXPOSE 8181
+#RUN unzip app.jar || true
+#ENTRYPOINT [ "java","org.springframework.boot.loader.launch.JarLauncher" ] 
+#\org\springframework\boot\loader\launch
+ENTRYPOINT [ "java","-jar","app.jar" ]
---
+++ src/main/java/com/tcs/sample_project/SampleProjectApplication.java
@@ -0,0 +1,21 @@
+package com.tcs.sample_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class SampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(){
+		return "<h1> Hello Welcome to Container </h1>";
+	}
+}
