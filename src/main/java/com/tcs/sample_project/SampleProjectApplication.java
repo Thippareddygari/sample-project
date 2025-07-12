0c16a03756a5d4ded3b604674ce31c307a10e765
kishor <kishorebsc12@gmail.com>
2025-07-12 19:45:12 +0530

    added

--- src/main/java/com/tcs/sample_project/SampleProjectApplication.java
+++ src/main/java/com/tcs/sample_project/SampleProjectApplication.java
@@ -16,6 +16,6 @@ public class SampleProjectApplication {
 
 	@GetMapping("/")
 	public String hello(){
-		return "<h1> Hello Welcome to Container </h1>";
+		return "<h1> Hello Welcome to Container test</h1>";
 	}
 }
